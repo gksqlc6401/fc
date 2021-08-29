a902b193a90c66725926e9b5e8fda7944cf1b088
gksqlc6401 <gksqlc6401@naver.com>
2021-08-29 19:30:33 +0900

    20210829

--- src/main/java/org/zerock/fc/annotations/Controller.java
+++ src/main/java/org/zerock/fc/annotations/Controller.java
@@ -8,8 +8,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)//클래스타입으로 하려고한다,메소드로하려면 타입을 메소드로 바꿔줘야함
+@Retention(RetentionPolicy.RUNTIME)//실행한다..?? 
 public @interface Controller {
 
     String value();
